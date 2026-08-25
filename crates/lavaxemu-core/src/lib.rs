854ab7fc4415ce85e8db77463bfc1b96399543ca
Aloys <jiangxinnju@163.com>
2026-08-25 22:13:25 +0800

    Implement LavaX indexed graphics engine

--- crates/lavaxemu-core/src/lib.rs
+++ crates/lavaxemu-core/src/lib.rs
@@ -1,7 +1,9 @@
+mod display;
 mod error;
 mod program;
 mod vm;
 
+pub use display::{BufferTarget, Display, DrawOperation, Transform};
 pub use error::{Error, Result};
 pub use program::{AddressWidth, GraphicsMode, LAV_HEADER_SIZE, LAV_MAGIC, Program, ProgramHeader};
 pub use vm::{EVALUATION_STACK_ADDRESS, GUEST_MEMORY_SIZE, RunOutcome, StepOutcome, Vm};
